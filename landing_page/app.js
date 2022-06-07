40eb2891e266825036e3899ed6dbf98211a44f86
usman479 <muusman479@gmail.com>
2022-06-08 00:29:15 +0500

    form complete 1.0

--- landing_page/app.js
+++ landing_page/app.js
@@ -34,6 +34,10 @@ function upload() {
     });
 }
 
+function closeModal(e){
+  
+}
+
 let sindhCities = ["karachi", "hyderabad", "sukkur", "nawabshah", "thatta"];
 let kpkCities = ["abbotabad", "peshawar", "mansehra", "mardan", "kohat"];
 let balochistanCities = ["quetta", "ziarat", "khuzdar", "gwadar", "jacobabad"];
@@ -90,26 +94,166 @@ inp.addEventListener("change", () => {
   }
 });
 
+let courseChart = document.querySelectorAll(".course-chart");
+let courseChart2 = document.querySelectorAll(".course-chart-02");
+let courseChart3 = document.querySelectorAll(".course-chart-03");
+let courseCharts = [courseChart,courseChart2,courseChart3];
 let count = 0;
-function papersCount() {
-  let countSpan = document.querySelector("#papers-count");
-  count = 0;
-  countSpan.innerHTML = "";
-  for (let i = 0; i < papers.length; i++) {
-    if (papers[i].value != "") {
-      ++count;
-    }
-  }
+
+// PAPERS COUNT //
+
+let attempt = document.getElementById("Attempt");
+let attempt2 = document.getElementById("Attempt02");
+let attempt3 = document.getElementById("Attempt03");
+let attemptSelect = document.getElementsByName("Attempt")[0];
+let attempt2Select = document.getElementsByName("Attempt02")[0];
+let attempt3Select = document.getElementsByName("Attempt03")[0];
+let paper01 = "";
+let paper02 = "";
+let paper03 = "";
+let flag1 = true;
+let flag2 = true;
+let flag3 = true;
+var courseModal01 = new bootstrap.Modal(document.getElementById('courseModal'), {
+  keyboard: true
+})
+var courseModal02 = new bootstrap.Modal(document.getElementById('courseModal02'), {
+  keyboard: true
+})
+var courseModal03 = new bootstrap.Modal(document.getElementById('courseModal03'), {
+  keyboard: true
+})
+let countSpan = document.querySelector("#papers-count");
+for(let i=0; i<courseChart.length; i++){
+  courseChart[i].addEventListener("click",(e) => {
+  if(flag1){
+    
+  ++count;
   countSpan.innerHTML = "0" + count;
+  attempt.classList.remove("hidden");
+  attemptSelect.setAttribute("required","required");
+  paper01 = e.target.getAttribute("value");
+  priceCalc();
+  courseModal01.toggle();
+  flag1 = false;
+  }
+  })
+}
+for(let i=0; i<courseChart.length; i++){
+  courseChart2[i].addEventListener("click",(e) => {
+    if(flag2){
+      let countSpan = document.querySelector("#papers-count");
+    ++count;
+    countSpan.innerHTML = "0" + count;
+    attempt2.classList.remove("hidden");
+    attempt2Select.setAttribute("required","required");
+    paper02 = e.target.getAttribute("value");
+    priceCalc();
+    courseModal02.toggle();
+    flag2 = false;
+    }
+  })
+}
+for(let i=0; i<courseChart.length; i++){
+  courseChart3[i].addEventListener("click",(e) => {
+    if(flag3){
+      let countSpan = document.querySelector("#papers-count");
+    ++count;
+    countSpan.innerHTML = "0" + count;
+    attempt3.classList.remove("hidden");
+    attempt3Select.setAttribute("required","required");
+    paper03 = e.target.getAttribute("value");
+    priceCalc();
+    courseModal03.toggle();
+    flag3 = false;
+    }
+  })
 }
 
-for (let i = 0; i < papers.length; i++) {
-  papers[i].addEventListener("change", papersCount);
+
+// function counter1 (e) {
+//   console.log(e.target);
+//   if(flag1){
+//     let countSpan = document.querySelector("#papers-count");
+//   ++count;
+//   countSpan.innerHTML = "0" + count;
+//   attempt.classList.remove("hidden");
+//   attemptSelect.setAttribute("required","required");
+//   priceCalc();
+//   flag1 = false;
+//   }
+// }
+// function counter2 () {
+//   if(flag2){
+//     let countSpan = document.querySelector("#papers-count");
+//   ++count;
+//   countSpan.innerHTML = "0" + count;
+//   attempt2.classList.remove("hidden");
+//   attempt2Select.setAttribute("required","required");
+//   paper02 = e.target.getAttribute("value");
+//   priceCalc();
+//   flag2 = false;
+//   }
+// }
+// function counter3 () {
+//   if(flag3){
+//     let countSpan = document.querySelector("#papers-count");
+//   ++count;
+//   countSpan.innerHTML = "0" + count;
+//   attempt3.classList.remove("hidden");
+//   attempt3Select.setAttribute("required","required");
+//   paper03 = e.target.getAttribute("value");
+//   priceCalc();
+//   flag3 = false;
+//   }
+// }
+
+
+
+
+let amountBox = document.querySelectorAll(".amount-box-2")[0];
+function priceCalc () {
+  if(count === 1){
+    amountBox.innerHTML = "&nbsp;&nbsp;25,000"
+  } else if (count === 2){
+    amountBox.innerHTML = "&nbsp;&nbsp;40,000"
+  } else if(count === 3){
+    amountBox.innerHTML = "&nbsp;&nbsp;50,000"
+  }
 }
+
+function discPriceCalc () {
+  if(count === 1){
+    amountBox.innerHTML = "&nbsp;&nbsp;20,000"
+  } else if (count === 2){
+    amountBox.innerHTML = "&nbsp;&nbsp;32,000"
+  } else if(count === 3){
+    amountBox.innerHTML = "&nbsp;&nbsp;40,000"
+  }
+}
+
+
+
+// COUPON //
+let couponSpan = document.querySelector("#coupon-span");
+let coupon = document.querySelector("#coupon");
+
+
+coupon.addEventListener("keyup", (e) => {
+  couponSpan.innerHTML = "";
+  if((cityInput.value+"20") === e.target.value){
+     couponSpan.innerHTML = "Discount Received";
+     discPriceCalc();
+  } else {
+    priceCalc();
+  }
+})
+
 
 // SLIDER //
 var slider = document.querySelector(".form-slider");
 var toggle = document.getElementsByClassName("toggle");
+var coursesRow = document.querySelector("#courses-row");
 for (let i = 0; i < toggle.length; i++) {
   toggle[i].addEventListener("click", toggleSlider, true);
 }
@@ -119,10 +263,12 @@ function toggleSlider() {
     slider.classList.remove("opened");
     slider.classList.add("closed");
     slider.classList.add("dp-none");
+    coursesRow.classList.remove("dp-none");
   } else {
     slider.classList.remove("closed");
     slider.classList.add("opened");
     slider.classList.remove("dp-none");
+    coursesRow.classList.add("dp-none");
   }
 }
 
@@ -130,6 +276,21 @@ function toggleSlider() {
 
 let cities = document.querySelector("#cities");
 let areas = document.querySelector("#Areas");
+let cityInput = document.querySelector("#city-input");
+let areaInput = document.querySelector("#area-input");
+let areaInputBox = document.getElementsByName("Area/Locality")[0];
+
+cityInput.addEventListener("keyup", (e) => {
+  if(e.target.value.toLowerCase() === "karachi") {
+    areaInput.classList.remove("dp-none")
+    areaInputBox.setAttribute("required","required");
+  }else if(e.target.classList.contains("dp-none") === false){
+    areaInput.classList.add("dp-none")
+    areaInputBox.removeAttribute("required");
+  }
+})
+
+
 cities.addEventListener("change", () => {
   areas.innerHTML = "";
   console.log(cities.value);
@@ -144,9 +305,21 @@ const scriptURL =
   "https://script.google.com/macros/s/AKfycbxngfvVQfWAr-XpcDx6oMnw9LhzlX-EzsElea20jYqpFhLaO16juRhMrSCzQts0fMGEtA/exec";
 let form = document.forms["registration-form"];
 
+
+
+let submitBtn = document.querySelector("#submit-btn");
+let loader = document.querySelectorAll(".loader")[0];
+
+function resetForm() {
+  form.reset();
+  count = 0;
+  countSpan.innerHTML = 0;
+  amountBox.innerHTML = 0;
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  
   let files = inp.files;
   if (files.length > 0) {
     if (files[0].size > 400 * 1024) {
@@ -164,6 +337,101 @@ form.addEventListener("submit", (e) => {
 
   form.Paper_Count.value = count;
 
+  if(count === 1){
+    loader.classList.remove("dp-none");
+    submitBtn.disabled = true;
+    form["Paper(s)"].value = paper01 || paper02 || paper03;
+    form.Attempt.value = form.Attempt.value || form.Attempt02.value || form.Attempt03.value;
+    storageRef
+      .child(name)
+      .put(file)
+      .then((snapshot) => {
+        storageRef
+          .child(name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log("1st");
+            form.ICAP_Card.value = url;
+          })
+          .then(() => {
+            console.log("2nd");
+            fetch(scriptURL, { method: "POST", body: new FormData(form) })
+              .then((response) => {
+                  loader.classList.add("dp-none");
+                    submitBtn.disabled = false;
+                    alert(
+                      "Thanks for Contacting us..! We Will Contact You Soon..."
+                    )
+                    resetForm();
+                  })
+                  .catch((error) => {
+                    console.error("Error!", error.message)
+                    loader.classList.add("dp-none");
+                    submitBtn.disabled = false;});
+                    resetForm();
+                    
+                  })
+                  .catch((error) => {
+                    console.error("Error!", error.message)
+                    loader.classList.add("dp-none");
+                    submitBtn.disabled = false;});
+                    resetForm();
+                    
+      });
+    
+  } else if (count === 2){
+    loader.classList.remove("dp-none");
+    submitBtn.disabled = true;
+    storageRef
+      .child(name)
+      .put(file)
+      .then((snapshot) => {
+        storageRef
+          .child(name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log("1st");
+            form.Attempt.value = form.Attempt.value || form.Attempt02.value;
+            form["Paper(s)"].value = paper01 || paper02;
+            form.ICAP_Card.value = url;
+          })
+          .then(() => {
+            console.log("2nd");
+            fetch(scriptURL, { method: "POST", body: new FormData(form) })
+              .then((response) => {
+                  console.log(form.Attempt.value);
+                //SECOND //
+                form.Attempt.value = form.Attempt03.value || form.Attempt02.value;
+                form["Paper(s)"].value = paper03 || paper02;
+                console.log(form.Attempt.value);
+                resetForm();
+                fetch(scriptURL, {
+                    method: "POST",
+                    body: new FormData(form),
+                  }).then((response) => {
+                    loader.classList.add("dp-none");
+                    submitBtn.disabled = false;
+                    alert(
+                      "Thanks for Contacting us..! We Will Contact You Soon..."
+                    )
+                    resetForm();
+                  });
+              })
+              .catch((error) => {
+                console.error("Error!", error.message)
+                loader.classList.add("dp-none");
+                submitBtn.disabled = false;});
+                resetForm();
+          })
+          .catch((error) => {
+            console.error("Error!", error.message)
+            loader.classList.add("dp-none");
+            submitBtn.disabled = false;});
+            resetForm();
+      });
+  } else if(count === 3){
+    loader.classList.remove("dp-none");
+  submitBtn.disabled = true;
   storageRef
     .child(name)
     .put(file)
@@ -173,6 +441,7 @@ form.addEventListener("submit", (e) => {
         .getDownloadURL()
         .then((url) => {
           console.log("FIRIST");
+          form["Paper(s)"].value = paper01;
           form.ICAP_Card.value = url;
         })
         .then(() => {
@@ -182,27 +451,49 @@ form.addEventListener("submit", (e) => {
                 console.log(form.Attempt.value);
               //SECOND //
               form.Attempt.value = form.Attempt02.value;
+              form["Paper(s)"].value = paper02;
               console.log(form.Attempt.value);
+              resetForm();
               fetch(scriptURL, {
                   method: "POST",
                   body: new FormData(form),
                 }).then((response) => {
                     // THIRD //
                     form.Attempt.value = form.Attempt03.value;
+                    form["Paper(s)"].value = paper01;
                     console.log(form.Attempt.value);
+                    resetForm();
                 fetch(scriptURL, { method: "POST", body: new FormData(form) })
-                  .then((response) =>
+                  .then((response) =>{
+                    loader.classList.add("dp-none");
+                    submitBtn.disabled = false;
                     alert(
                       "Thanks for Contacting us..! We Will Contact You Soon..."
-                    )
+                    );
+                    resetForm();
+                  }
                   )
-                  .catch((error) => console.error("Error!", error.message));
+                  .catch((error) => {
+                    console.error("Error!", error.message)
+                    loader.classList.add("dp-none");
+                    submitBtn.disabled = false;
+                    resetForm();});
               });
             })
-            .catch((error) => console.error("Error!", error.message));
+            .catch((error) => {
+              console.error("Error!", error.message)
+              loader.classList.add("dp-none");
+              submitBtn.disabled = false;
+              resetForm();});
         })
-        .catch((error) => console.error("Error!", error.message));
+        .catch((error) => {
+          console.error("Error!", error.message)
+          loader.classList.add("dp-none");
+          submitBtn.disabled = false;
+          resetForm();});
     });
+  }
+  
 });
 
 
