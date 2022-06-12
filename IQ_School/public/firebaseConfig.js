24a0bb233e3ac7dc7e39b5b36ccf76e26a8ebf95
usman479 <muusman479@gmail.com>
2022-06-12 14:02:14 +0500

    payment modal

---
+++ IQ_School/public/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIU5tdRe4Fvm0_8ktwR6XWe0yBtPADv6g",
+  authDomain: "landing-page-e94ed.firebaseapp.com",
+  projectId: "landing-page-e94ed",
+  storageBucket: "landing-page-e94ed.appspot.com",
+  messagingSenderId: "929767843654",
+  appId: "1:929767843654:web:683ef0d1f6a9e4852de747",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
