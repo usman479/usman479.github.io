84d4742ebef4119f3900506d71eb89ef4ffba948
usman479 <79809127+usman479@users.noreply.github.com>
2022-12-03 17:03:31 +0500

    GDSC round 2 task

---
+++ personal-portfolio-website/scripts/app.js
@@ -0,0 +1,14 @@
+document.querySelector(".cross").style.display = "none";
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".sidebar").classList.toggle("sidebar-go");
+    if(document.querySelector(".sidebar").classList.contains("sidebar-go")) {
+        document.querySelector(".ham").style.display = "inline";        
+        document.querySelector(".cross").style.display = "none";        
+    } else {
+        document.querySelector(".ham").style.display = "none";        
+        setTimeout(() => {
+            document.querySelector(".cross").style.display = "inline";   
+        }, 300)
+    }
+})
